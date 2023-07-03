0b2eb549458f6aa105c042e6d7f8e30402e96d5a
AndrewHemry <Andrewhemry@gmail.com>
2023-07-02 20:54:36 -0500

    Updating with a function to close the window and display the access token

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ async function getAuthorizationCode(req, res) {
     try {
         // Make the POST request to the external API with the authorizationCode
         const response = await getAccessToken(authorizationCode);
-        console.log("Inside the try")
+        console.log("Access Token:", response)
+
         // Send the response from the external API back to the client
-        // res.json(response.data);
+        // res.send({access_token: response})
+        // res.json(response)
+        res.send(`<script>window.opener.postMessage({ type: 'response', data: '${response}' }, 'http://localhost:3000');</script>`)
     } catch (error) {
         console.log("The error is", error);
         res.status(500).json({ error: "Internal server error" });
@@ -27,34 +30,44 @@ async function getAuthorizationCode(req, res) {
 }
 
 async function getAccessToken(authorizationCode) {
-    let newWindow;
 
-    let data = JSON.stringify({
-        "grant_type": "authorization_code",
-        "client_id": "096c981e58e4d75140a23d96312dc94857e74dfa492161e8712f617edc4849f5",
-        "client_secret": process.env.REACT_APP_API_SECRET,
-        "redirect_uri": "http://localhost:8000/callback",
-        "code": authorizationCode
-    });
+    try {
+        let data = JSON.stringify({
+            "grant_type": "authorization_code",
+            "client_id": "096c981e58e4d75140a23d96312dc94857e74dfa492161e8712f617edc4849f5",
+            "client_secret": process.env.REACT_APP_API_SECRET,
+            "redirect_uri": "http://localhost:8000/callback",
+            "code": authorizationCode
+        });
 
-    let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: 'https://login-sandbox.procore.com/oauth/token',
-        headers: { 
-            'Content-Type': 'application/json', 
-        },
-        data : data
-    };
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://login-sandbox.procore.com/oauth/token',
+            headers: { 
+                'Content-Type': 'application/json', 
+            },
+            data : data
+        };
 
-    axios.request(config)
-        .then((response) => {
-            console.log(JSON.stringify(response.data));
-            console.log("Response Data")
-        })
-        .catch((error) => {
-            console.log(error);
-    });
+        const response = await axios.request(config)
+
+        return response.data.access_token;
+    } catch (error) {
+        console.log("Encountered an error", error)
+        throw new Error("Failed to retrieve access token")
+    }
+
+    // axios.request(config)
+    //     .then((response) => {
+    //         console.log(JSON.stringify(response.data));
+    //         console.log("Response Data")
+            
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //         throw new Error("Failed to retrieve access token")
+    // });
 }
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Login = (props) => {
     const urlCode = new URL("https://login-sandbox.procore.com/oauth/authorize?client_id=096c981e58e4d75140a23d96312dc94857e74dfa492161e8712f617edc4849f5&response_type=code&redirect_uri=http://localhost:8000/callback");
     let newWindow;
+    const [buttonClicked, setButtonClicked] = useState(false)
+    const [accessToken, setAccessToken] = useState('')
+
+    useEffect(() => {
+        window.addEventListener("message", handlePopupResponse);
+
+        // May not need the code below
+        // return () => {
+        //     window.removeEventListener("message", handlePopupResponse)
+        // };
+    }, []);
+
+    const handlePopupResponse = (event) => {
+        const { type, data } = event.data
+
+        if (type == "response" && data != null) {
+            setAccessToken(data) //Set the state for the access token
+            newWindow.close()
+        }        
+    }
 
     const handleLogin = () => {
         console.log("Login was pressed")
         authorizationPopUp()
+        setButtonClicked(true) //Set the state to true
     }
 
     const authorizationPopUp = () => {
@@ -20,6 +41,14 @@ const Login = (props) => {
     return (
         <div>
             <button onClick={handleLogin}>Login with Procore</button>
+            <p>The Button Clicked State: {buttonClicked.toString()}</p>
+            <div>
+                {accessToken ? (
+                    <p>The Access Token: {accessToken}</p>
+                ) : (
+                    <p>Waiting for Access Token...</p>
+                )}
+            </div>
         </div>
     )
 }
